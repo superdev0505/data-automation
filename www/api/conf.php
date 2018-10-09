33c04c0078edf8dd58289619a371cb0f504cce99
superdev0505 <grantlatest@gmail.com>
2018-10-09 17:32:07 +0200

    source

---
+++ www/api/conf.php
@@ -0,0 +1,9 @@
+<?php
+define('DATABASE_HOST','127.0.0.1');
+define('DATABASE_NAME','oil');
+define('DATABASE_USER','root');
+define('DATABASE_PASS','Mngt98HY!df^7h');
+//define('DATABASE_PASS','root');
+
+
+?>
---
+++ www/nymex/1/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$use_proxy = "false";
+// database configuration
+$servername = "localhost";
+$username = "root";
+$password = "Mngt98HY!df^7h";
+$dataBaseName = "nymex";
+$DB_tableName1 = "price_table_future";
+$DB_tableName2 = "price_table_options";
